10fee596efe3cb44f3648daadf68c0905d3d43b5
Akshay Jyothi.P <akshayjyothip@gmail.com>
2024-11-05 19:07:46 +0530

    add middleware and function to handle error

--- api/controllers/auth.conteoller.js
+++ api/controllers/auth.conteoller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.modal.js";
 import becryptjs from'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
 
    //need to save this data to database
 
@@ -14,7 +15,6 @@ export const signup=async(req,res)=>{
       await  newUser.save()   //here we use await bcs tell js to untill geting the data stay in this line
       res.status(201).json({message:"user created successfully"})
    } catch (error) {
-      res.status(500).json(error.message); 
+next(error)
    }
-
 }
